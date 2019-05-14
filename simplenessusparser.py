ea20ab45044f037bfdafd03b5b8ce6ffaae337dc
Francesco <47717886+4n4nk3@users.noreply.github.com>
2019-05-14 16:33:50 +0200

    Update simplenessusparser.py total_vulns
    Substituted cycle to count the 'total_vulns' variable containing the total number of vulnerabilities (Low, Medium, High, Critical) with a simple += 1 operation.

--- simplenessusparser.py
+++ simplenessusparser.py
@@ -13,6 +13,7 @@ outdated_count = 0
 with open('test.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     counter = 0
+    total_vulns = 0
     for row in csv_reader:
         if counter == 0:
             print('Column names are '.format(",".join(row)))
@@ -30,6 +31,7 @@ with open('test.csv') as csv_file:
                 networks_hosts_count[network]['Hosts'] += 1
             networks_hosts_count[network][risk] += 1
             if risk != 'None':
+                total_vulns += 1
                 if port != '0':
                     if port not in ports_protocols:
                         ports_protocols[port] = {}
@@ -60,10 +62,6 @@ else:
     print('\n\nScanned {} hosts in {} networks'.format(len(all_hosts), len(networks_hosts_count)))
 
 for network in networks_hosts_count:
-    total_vulns = 0
-    for x in networks_hosts_count[network]:
-        if x != 'None' and x != 'Hosts':
-            total_vulns += networks_hosts_count[network][x]
     print(
         '\n\n\nNetwork {}.X\t\tHosts:\t{}\t\tInfo:\t{}\n\n\tLow:\t\t{}\n\tMedium:\t\t{}\n\tHigh:\t\t{'
         '}\n\tCritical:\t{}\n\n\tTotal vulns: {}'.format(
@@ -72,14 +70,14 @@ for network in networks_hosts_count:
             networks_hosts_count[network]['Medium'], networks_hosts_count[network]['High'],
             networks_hosts_count[network]['Critical'], total_vulns))
 
-print('\n\n\nTotal vulnerabilities by port:\n')
-ports_protocols = OrderedDict(sorted(ports_protocols.items(), key=lambda x:int(x[0])))
+print('\n\n\nVulnerabilities by port:\n')
+ports_protocols = OrderedDict(sorted(ports_protocols.items(), key=lambda x: int(x[0])))
 for port in ports_protocols:
     for protocol in ports_protocols[port]:
         print('\t{}\t{}  \t\t--> {}'.format(port, protocol, ports_protocols[port][protocol]))
 
 print('\n\n\nCritical / High vulnerabilities by port:\n')
-high_critical_ports_protocols = OrderedDict(sorted(high_critical_ports_protocols.items(), key=lambda x:int(x[0])))
+high_critical_ports_protocols = OrderedDict(sorted(high_critical_ports_protocols.items(), key=lambda x: int(x[0])))
 for port in high_critical_ports_protocols:
     for protocol in high_critical_ports_protocols[port]:
         print('\t{} {}  \t\t--> {}'.format(port, protocol, high_critical_ports_protocols[port][protocol]))
